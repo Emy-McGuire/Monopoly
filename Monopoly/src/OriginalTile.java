793f47e008be74ad98622845dc2f9e7a2016ea06
emdeline <emdeline@10.0.0.154>
2020-02-25 18:55:40 -0700

    I made the arraylists

---
+++ Monopoly/src/OriginalTile.java
@@ -0,0 +1,17 @@
+public class OriginalTile {
+	private String name;
+	
+	public OriginalTile(String n)
+	{
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Monopoly/src/OriginalTileArrayList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class OriginalTileArrayList {
+static ArrayList <OriginalTile> originalNames = new ArrayList<OriginalTile>();
+	public static void main(String[] args) {
+
+		originalNames.add(new OriginalTile("GO"));
+		originalNames.add(new OriginalTile("MEDITERRANEAN AVENUE"));
+		originalNames.add(new OriginalTile("COMMUNITY CHEST"));
+		originalNames.add(new OriginalTile("BALTIC AVENUE"));
+		originalNames.add(new OriginalTile("INCOME TAX"));
+		originalNames.add(new OriginalTile("READING RAILROAD"));
+		originalNames.add(new OriginalTile("ORIENTAL AVENUE"));
+		originalNames.add(new OriginalTile("CHANCE"));
+		originalNames.add(new OriginalTile("VERMONT AVENUE"));
+		originalNames.add(new OriginalTile("CONNECTICUT AVENUE"));
+		originalNames.add(new OriginalTile("IN JAIL/VISITING"));
+		originalNames.add(new OriginalTile("ST. CHARLES PLACE"));
+		originalNames.add(new OriginalTile("ELECTRIC COMPANY"));
+		originalNames.add(new OriginalTile("STATES AVENUE"));
+		originalNames.add(new OriginalTile("VIRGINIA AVENUE"));
+		originalNames.add(new OriginalTile("PENNSYLVANIA RAILROAD"));
+		originalNames.add(new OriginalTile("ST. JAMES PLACE"));
+		originalNames.add(new OriginalTile("COMMUNITY CHEST"));
+		originalNames.add(new OriginalTile("TENNESSEE AVENUE"));
+		originalNames.add(new OriginalTile("NEW YORK AVENUE"));
+		originalNames.add(new OriginalTile("FREE PARKING"));
+		originalNames.add(new OriginalTile("KENTUCKY AVENUE"));
+		originalNames.add(new OriginalTile("CHANCE"));
+		originalNames.add(new OriginalTile("INDIANA AVENUE"));
+		originalNames.add(new OriginalTile("ILLINOIS AVENUE"));
+		originalNames.add(new OriginalTile("B&O RAILROAD"));
+		originalNames.add(new OriginalTile("ATLANTIC AVENUE"));
+		originalNames.add(new OriginalTile("VENTNOR AVENUE"));
+		originalNames.add(new OriginalTile("WATER WORKS"));
+		originalNames.add(new OriginalTile("MARVIN GARDENS"));
+		originalNames.add(new OriginalTile("GO TO JAIL"));
+		originalNames.add(new OriginalTile("PACIFIC AVENUE"));
+		originalNames.add(new OriginalTile("NORTH CAROLINA AVENUE"));
+		originalNames.add(new OriginalTile("COMMUNITY CHEST"));
+		originalNames.add(new OriginalTile("PENNSYLVANIA AVENUE"));
+		originalNames.add(new OriginalTile("SHORT LINE"));
+		originalNames.add(new OriginalTile("CHANCE"));
+		originalNames.add(new OriginalTile("PARK PLACE"));
+		originalNames.add(new OriginalTile("LUXURY TAX"));
+		originalNames.add(new OriginalTile("BOARD WALK"));
+
+		
+	}
+
+}
